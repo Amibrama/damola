abd77ace38d54237594eddfea8332c6110ed22b6
Dmambo <mambotrader0@gmail.com>
2023-06-19 18:47:37 +0530

    Style the project section

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,36 +1,34 @@
 import React from "react";
 import "./Project.css";
-import phone from "../../assets/phone.png";
-import laptop from "../../assets/laptop.png";
-import cloth from "../../assets/cloth.png";
-import smiks from "../../assets/smiks.png";
 import ProjectData from "./ProjectData";
 
 const Project = () => {
   return (
     <div className="project">
-      <h1>projects</h1>
-      <div className="images">
-        <img src={cloth} alt="ptimages" />
-        <img src={laptop} alt="ptimages" />
-        <img src={smiks} alt="ptimages" />
-        <img src={phone} alt="ptimages" />
-      </div>
-
       <div className="projects">
-        <h1> Recent works</h1>
-
-        {ProjectData.map((data) => (
-          <div className="project__card" key={data.id}>
-            <div className="projectimage">
-              <img src={data.image} alt="project" />
-            </div>
-            <div className="project__info">
-              <h2>{data.title}</h2>
-              <p>{data.desc}</p>
-            </div>
+        <h1>Recent works</h1>
+        <div className="project__card1" key={ProjectData[0].id}>
+          <div className="projectimage">
+            <img src={ProjectData[0].image} alt="project" />
           </div>
-        ))}
+          <div className="project__info">
+            <h2>{ProjectData[0].title}</h2>
+            <p>{ProjectData[0].desc}</p>
+          </div>
+        </div>
+        <div className="project__cards">
+          {ProjectData.slice(1).map((data) => (
+            <div className="project__card" key={data.id}>
+              <div className="projectimage">
+                <img src={data.image} alt="project" />
+              </div>
+              <div className="project__info">
+                <h2>{data.title}</h2>
+                <p>{data.desc}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Project/ProjectData.jsx
+++ src/components/Project/ProjectData.jsx
@@ -1,5 +1,10 @@
 const ProjectData = [
   {
+    image: "./cloths.png",
+    title: "STYLEZ",
+    desc: "Stylez is a fashion app that simplifies the process of putting together stylish outfits, even when time is limited. Discover a curated collection of clothing and accessories, mix and match pieces effortlessly, and create stunning looks in minutes. Elevate your style with Stylez and enjoy effortless fashion at your fingertips.",
+  },
+  {
     image: "./laptop.png",
     title: "Ade's Rack",
     desc: "Ade's Rack is a unique fashion website designed to empower individuals to effortlessly create stylish outfits, even when pressed for time. Our platform provides users with the tools and inspiration to curate fabulous ensembles, ensuring they look their best without the stress. With a wide range of clothing options, accessories, and style recommendations, Ade's Rack is the go-to destination for fashion enthusiasts seeking convenience and sartorial excellence. Join our fashion community and unlock your full style potential with Ade's Rack.",
@@ -13,11 +18,6 @@ const ProjectData = [
     image: "./africa.png",
     title: "Eat Africa",
     desc: "The Eat African app is your gateway to experiencing the rich and diverse flavors of African cuisine. Discover, order, and enjoy authentic African dishes from local restaurants and home chefs with just a few taps on your mobile device. From mouthwatering stews to tantalizing grilled meats and delightful desserts, the app connects you with the vibrant world of African food, promoting cultural exchange and celebrating the culinary heritage of Africa. Download Eat African and embark on a flavorful journey today.",
-  },
-  {
-    image: "./cloths.png",
-    title: "Ade's Clothing",
-    desc: "Ade's Rack is a fashion app that simplifies the process of putting together stylish outfits, even when time is limited. Discover a curated collection of clothing and accessories, mix and match pieces effortlessly, and create stunning looks in minutes. Elevate your style with Ade's Rack and enjoy effortless fashion at your fingertips.",
   },
 ];
 
